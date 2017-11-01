125858289569e48d91e4734027415064c8fdd753
rhpvorderman <r.h.p.vorderman@lumc.nl>
2017-11-01 15:28:50 +0100

    simplified testArgs

--- src/main/scala/nl/biopet/tools/fastqsync/FastqSync.scala
+++ src/main/scala/nl/biopet/tools/fastqsync/FastqSync.scala
@@ -12,9 +12,7 @@ object FastqSync extends ToolCommand[Args] {
   def emptyArgs: Args = Args()
   def argsParser = new ArgsParser(toolName)
   def main(args: Array[String]): Unit = {
-    val parser = new ArgsParser(toolName)
-    val cmdArgs =
-      parser.parse(args, Args()).getOrElse(throw new IllegalArgumentException)
+    val cmdArgs = cmdArrayToArgs(args)
 
     logger.info("Start")
 
